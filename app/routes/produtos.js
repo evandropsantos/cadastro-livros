70660a32295063802360f5c08e8fb2680747e106
Evandro P. Santos <evandro.santos@multilaser.com.br>
2017-09-20 17:41:11 -0300

    Adicionando classe DAO

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,12 +3,23 @@ module.exports = function(app) {
     app.get('/produtos', function(req, res) {
 
         var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco;
+        var produtosBanco = new app.infra.ProdutosDAO(connection);
         
-        produtosBanco.lista(connection, function(erros, resultados) {
+        produtosBanco.lista(function(erros, resultados) {
             res.render('produtos/lista', {lista: resultados});
         });
     
         connection.end();
+    });
+
+    app.get('produtos/remove', function() {
+
+        var connection = app.infra.connectionFactory();
+        var produtosBanco = app.infra.produtosBanco(connection);
+        var produto = produtosBanco.carrega(id, callback);
+
+        if(produto) {
+            produtosBanco.remove(produto, callback);
+        }
     });
 }
