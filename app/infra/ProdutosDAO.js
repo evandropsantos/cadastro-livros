db016ab590269b7a12cc6254c18e221c7703a40d
Evandro P. Santos <evandro.santos@multilaser.com.br>
2017-09-20 18:40:39 -0300

    Alternativa inset

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -10,6 +10,10 @@ ProdutosDAO.prototype.salva = function(produto, callback) {
     this._connection.query('insert into livros set ?', produto, callback);
 }
 
+//ProdutosDAO.prototype.salva = function (produto, callback) {
+  //  this._connection.query('insert into produtos (titulo, preco, descricao) values (?, ?, ?)',  [produto.titulo, produto.preco, produto.descricao], callback);
+//}
+
 module.exports = function() {
     return ProdutosDAO;
 }
