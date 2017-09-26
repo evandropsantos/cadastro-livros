92263c55adac67dd371adff70bf98d003313e2c4
Evandro P. Santos <evandro.santos@multilaser.com.br>
2017-09-26 17:38:42 -0300

    Validacoes do formulario

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -23,11 +23,21 @@ module.exports = function(app) {
     app.post('/produtos', function(req, res) {
         
         var produto = req.body;
-        
-        console.log(produto);
-        
+
+        req.assert('titulo', 'Titulo e obrigatorio.').notEmpty();
+        req.assert('preco', 'Formato invalido').isFloat();
+
+        var erros = req.validationErrors();
+        if(erros) {
+            res.render('produtos/form', {errosValidacao: erros});
+
+            return;
+        }
+
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
+        
+        console.log(produto);
         
         produtosDAO.salva(produto, function(erros, resultados) {
             
@@ -39,6 +49,6 @@ module.exports = function(app) {
 
     app.get('/produtos/form', function(req, res) {
 
-        res.render('produtos/form');
+        res.render('produtos/form', { errosValidacao: {} });
     });
 }
--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = function() { 
     
@@ -11,6 +12,7 @@ module.exports = function() {
 
     app.use( bodyParser.urlencoded({extended: true}) );
     app.use( bodyParser.json() );
+    app.use( expressValidator() );
 
     consign({cwd: 'app'})
         .include('routes')
