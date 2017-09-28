b25c00a8d6a3769090510ec4c25ffa93f5dcb688
Evandro P. Santos <evandro.santos@multilaser.com.br>
2017-09-28 12:25:38 -0300

    Adicionando asserts ao teste

--- test/produtos.js
+++ test/produtos.js
@@ -1,6 +1,26 @@
+var http = require('http');
+var assert = require('assert');
+
 describe('ProdutosController', function() {
 
-    it('#Listagem teste', function() {
-        console.log('Teste de verificacao de listagem json.');
+    it('#Listagem JSON', function(done) {
+        
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+
+        http.get(configuracoes, function(res) {
+
+            assert.equal(res.statusCode, 200);
+
+            assert.equal(res.headers['content-type'], 'application/json');
+
+            done();
+        });
     });
 });
