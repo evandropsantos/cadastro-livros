532db2236ecfe59831664b1c75d8e0a20b6b47d7
Evandro P. Santos <evandro.santos@multilaser.com.br>
2017-10-30 18:34:02 -0200

    Cadastro de promocoes

---
+++ app/routes/promocoes.js
@@ -0,0 +1,24 @@
+module.exports = function(app) {
+    
+    app.get("/promocoes/form", function(req, res) {
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.lista( function(error, results) {
+
+            res.render('promocoes/form', { lista:results });
+        });
+
+        connection.end();
+    });
+
+    app.post("/promocoes", function(req, res) {
+        
+        var promocao = req.body;
+
+        console.log(promocao);
+
+        res.redirect('promocoes/form');
+    });
+}
